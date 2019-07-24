66e7870cbdc4788bfb0cd51a94deaa3961ebfe42
r4v3n6101 <raven6107@gmail.com>
2019-07-24 04:57:47 +0300

    Update archive name

--- src/main/java/properties/Constants.java
+++ src/main/java/properties/Constants.java
@@ -3,5 +3,5 @@ package properties;
 public class Constants {
     public static final String MODID = "mc-properties";
     public static final String NAME = MODID;
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
 }
